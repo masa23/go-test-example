67a3377ac7242ed7a19fc6aa2cb902afe08fd4b0
Hiroaki Nakamura <hnakamur@gmail.com>
2016-12-27 18:13:07 +0900

    Revert "Add TestFormat"
    This reverts commit 40d72a44e8a7f77b2d4f5316be2fd8f1620973d7.

--- time_test.go
+++ time_test.go
@@ -29,42 +29,42 @@ func TestRFC3339Conversion(t *testing.T) {
 	}
 }
 
-type FormatTest struct {
-	name   string
-	format string
-	result string
-}
-
-var formatTests = []FormatTest{
-	{"ANSIC", time.ANSIC, "Wed Feb  4 21:00:57 2009"},
-	{"UnixDate", time.UnixDate, "Wed Feb  4 21:00:57 PST 2009"},
-	{"RubyDate", time.RubyDate, "Wed Feb 04 21:00:57 -0800 2009"},
-	{"RFC822", time.RFC822, "04 Feb 09 21:00 PST"},
-	{"RFC850", time.RFC850, "Wednesday, 04-Feb-09 21:00:57 PST"},
-	{"RFC1123", time.RFC1123, "Wed, 04 Feb 2009 21:00:57 PST"},
-	{"RFC1123Z", time.RFC1123Z, "Wed, 04 Feb 2009 21:00:57 -0800"},
-	{"RFC3339", time.RFC3339, "2009-02-04T21:00:57-08:00"},
-	{"RFC3339Nano", time.RFC3339Nano, "2009-02-04T21:00:57.0123456-08:00"},
-	{"Kitchen", time.Kitchen, "9:00PM"},
-	{"am/pm", "3pm", "9pm"},
-	{"AM/PM", "3PM", "9PM"},
-	{"two-digit year", "06 01 02", "09 02 04"},
-	// Three-letter months and days must not be followed by lower-case letter.
-	{"Janet", "Hi Janet, the Month is January", "Hi Janet, the Month is February"},
-	// Time stamps, Fractional seconds.
-	{"Stamp", time.Stamp, "Feb  4 21:00:57"},
-	{"StampMilli", time.StampMilli, "Feb  4 21:00:57.012"},
-	{"StampMicro", time.StampMicro, "Feb  4 21:00:57.012345"},
-	{"StampNano", time.StampNano, "Feb  4 21:00:57.012345600"},
-}
-
-func TestFormat(t *testing.T) {
-	// The numeric time represents Thu Feb  4 21:00:57.012345600 PST 2010
-	time_ := time.Unix(0, 1233810057012345600)
-	for _, test := range formatTests {
-		result := time_.Format(test.format)
-		if result != test.result {
-			t.Errorf("%s expected %q got %q", test.name, test.result, result)
-		}
-	}
-}
+// type FormatTest struct {
+// 	name   string
+// 	format string
+// 	result string
+// }
+//
+// var formatTests = []FormatTest{
+// 	{"ANSIC", time.ANSIC, "Wed Feb  4 21:00:57 2009"},
+// 	{"UnixDate", time.UnixDate, "Wed Feb  4 21:00:57 PST 2009"},
+// 	{"RubyDate", time.RubyDate, "Wed Feb 04 21:00:57 -0800 2009"},
+// 	{"RFC822", time.RFC822, "04 Feb 09 21:00 PST"},
+// 	{"RFC850", time.RFC850, "Wednesday, 04-Feb-09 21:00:57 PST"},
+// 	{"RFC1123", time.RFC1123, "Wed, 04 Feb 2009 21:00:57 PST"},
+// 	{"RFC1123Z", time.RFC1123Z, "Wed, 04 Feb 2009 21:00:57 -0800"},
+// 	{"RFC3339", time.RFC3339, "2009-02-04T21:00:57-08:00"},
+// 	{"RFC3339Nano", time.RFC3339Nano, "2009-02-04T21:00:57.0123456-08:00"},
+// 	{"Kitchen", time.Kitchen, "9:00PM"},
+// 	{"am/pm", "3pm", "9pm"},
+// 	{"AM/PM", "3PM", "9PM"},
+// 	{"two-digit year", "06 01 02", "09 02 04"},
+// 	// Three-letter months and days must not be followed by lower-case letter.
+// 	{"Janet", "Hi Janet, the Month is January", "Hi Janet, the Month is February"},
+// 	// Time stamps, Fractional seconds.
+// 	{"Stamp", time.Stamp, "Feb  4 21:00:57"},
+// 	{"StampMilli", time.StampMilli, "Feb  4 21:00:57.012"},
+// 	{"StampMicro", time.StampMicro, "Feb  4 21:00:57.012345"},
+// 	{"StampNano", time.StampNano, "Feb  4 21:00:57.012345600"},
+// }
+//
+// func TestFormat(t *testing.T) {
+// 	// The numeric time represents Thu Feb  4 21:00:57.012345600 PST 2010
+// 	time_ := time.Unix(0, 1233810057012345600)
+// 	for _, test := range formatTests {
+// 		result := time_.Format(test.format)
+// 		if result != test.result {
+// 			t.Errorf("%s expected %q got %q", test.name, test.result, result)
+// 		}
+// 	}
+// }
